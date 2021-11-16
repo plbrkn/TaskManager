ff1ff53653d81effd47614748f1d7a224e0f49f5
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2021-11-16 13:52:58 +0800

    add task show

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -7,4 +7,9 @@ class Api::V1::TasksController < Api::V1::ApplicationController
               .per(per_page)
   respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
  end
+
+ def show
+  task = Task.find(params[:id])
+  respond_with(task, serializer: TaskSerializer)
+ end
 end
