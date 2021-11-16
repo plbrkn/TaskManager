c4d2aca23018895b9a13fb928ea909ab5f21695c
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2021-11-16 13:44:20 +0800

    add api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,12 @@ Rails.application.routes.draw do
     resources :users
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :tasks, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
+
   scope module: :web do
     resource :board, only: :show
     resource :session, only: [:new, :create, :destroy]
